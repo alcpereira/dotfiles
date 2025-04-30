1905bffd8048abc8167399f7f5773f93a9e16cc4
Alexandre Pereira <alexandre.carneiro.pereira@gmail.com>
2025-04-30 20:32:09 +0100

    remove gem and gem path

--- .config/zsh/react-native.zsh
+++ .config/zsh/react-native.zsh
@@ -1,7 +1,5 @@
 export JAVA_HOME="/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home/"
 export PATH="/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home/bin:$PATH"
 export ANDROID_HOME=$HOME/Library/Android/sdk
-export GEM_HOME=$HOME/.gem
-export PATH="$GEM_HOME/bin:$PATH"
 export PATH="$HOME/.rbenv/shims:$PATH"
 eval "$(rbenv init - zsh)"
