c97b5e0c09962a2556055da8405930460804cdac
Alexandre Pereira <alexandre.carneiro.pereira@gmail.com>
2025-05-01 14:13:56 +0100

    nvim config update deprecated tbl_flatten

--- .config/nvim/lua/plugins/dump.lua
+++ .config/nvim/lua/plugins/dump.lua
@@ -220,18 +220,21 @@ return {
               table.insert(args, pieces[2])
             end
 
-            return vim.tbl_flatten({
-              args,
-              {
-                "--color=never",
-                "--no-heading",
-                "--with-filename",
-                "--line-number",
-                "--column",
-                "--smart-case",
-                "--hidden",
-              },
-            })
+            return vim
+              .iter({
+                args,
+                {
+                  "--color=never",
+                  "--no-heading",
+                  "--with-filename",
+                  "--line-number",
+                  "--column",
+                  "--smart-case",
+                  "--hidden",
+                },
+              })
+              :flatten()
+              :totable()
           end,
           cwd = opts.cwd,
           entry_maker = make_entry.gen_from_vimgrep(opts),
