d8401bd4677dbd8cad599a11d5a26614cce8b789
Alexandre Pereira <alexandre.carneiro.pereira@gmail.com>
2025-05-03 19:55:55 +0100

    add android sdk tools path to zsh config

--- .config/zsh/react-native.zsh
+++ .config/zsh/react-native.zsh
@@ -1,5 +1,7 @@
 export JAVA_HOME="/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home/"
 export PATH="/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home/bin:$PATH"
 export ANDROID_HOME=$HOME/Library/Android/sdk
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/platform-tools
 export PATH="$HOME/.rbenv/shims:$PATH"
 eval "$(rbenv init - zsh)"
