946451d2668e34969d91dad7d7a179e71d032cbb
Alexandre Pereira <alexandre.carneiro.pereira@gmail.com>
2025-05-08 16:54:36 +0100

    zsh react native remove duplicated rbenv - it is in zprofile

--- .config/zsh/react-native.zsh
+++ .config/zsh/react-native.zsh
@@ -3,5 +3,3 @@ export PATH="/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home/bin:$PA
 export ANDROID_HOME=$HOME/Library/Android/sdk
 export PATH=$PATH:$ANDROID_HOME/emulator
 export PATH=$PATH:$ANDROID_HOME/platform-tools
-export PATH="$HOME/.rbenv/shims:$PATH"
-eval "$(rbenv init - zsh)"
