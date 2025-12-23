c23dd39ae6fb43db78fd69b85fd7664ba949eddb
Alexandre Pereira <alexandre.carneiro.pereira@gmail.com>
2025-12-23 20:50:16 +0000

    remove copilotchat

--- nvim/.config/nvim/lua/plugins/ai.lua
+++ nvim/.config/nvim/lua/plugins/ai.lua
@@ -1,13 +1,5 @@
 return {
   {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    dependencies = {
-      { "github/copilot.vim" },
-      { "nvim-lua/plenary.nvim", branch = "master" },
-    },
-    build = "make tiktoken",
-    opts = {
-      model = "gpt-4o",
-    },
+    "github/copilot.vim",
   },
 }
