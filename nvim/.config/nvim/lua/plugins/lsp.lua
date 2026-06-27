ba3f291bf5c8312d2cd340d38ec93c4814f7ad24
Alexandre Pereira <alexandre.carneiro.pereira@gmail.com>
2026-06-27 16:06:16 +0100

    nvim remove fidget and replace with mini.notify

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -19,11 +19,6 @@ return {
       { "williamboman/mason.nvim", opts = {} },
       "williamboman/mason-lspconfig.nvim",
       "WhoIsSethDaniel/mason-tool-installer.nvim",
-
-      -- Useful status updates for LSP.
-      { "j-hui/fidget.nvim", opts = {} },
-
-      -- Allows extra capabilities provided by nvim-cmp
       "saghen/blink.cmp",
     },
     config = function()
